140865b7a1afca5c4c4597d681a93eef5e13bbd8
0Clark30 <austinlaw.clark@outlook.com>
2024-02-08 11:18:07 -0600

    pre merge

--- models/User.js
+++ models/User.js
@@ -1,1 +1,70 @@
 // main model for the user, Routines will extend this (one user hasMany Routines)
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+class User extends Model {
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [6],
+      },
+    },
+    heightInches{
+        type: DataTypes.NUMBER,
+        allowNull: false,
+    },
+    weightLbs{
+        type: DataTypes.NUMBER,
+        allowNull:false
+    },
+    age{
+        type: DataTypes.NUMBER,
+        allowNull: false
+    }
+    gender{
+        type: DataTypes.NUMBER,
+    }
+  },
+  {
+    hooks: {
+      async beforeCreate(newUserData) {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user',
+  }
+);
+
+module.exports = User;

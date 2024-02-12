5d02bc8c69286389bb7ba5f9d5176c24189dc35c
0Clark30 <austinlaw.clark@outlook.com>
2024-02-12 12:01:38 -0600

    added advanced routines pngs

---
+++ seeds/routinesData.js
@@ -0,0 +1,39 @@
+const {Routines} = require('../models');
+
+const routinesData = [
+  {
+    routine_name: "Beginner",
+    filename: ,
+    user_id: {},
+  },
+  {
+    routine_name: "Intermediate Strength",
+    filename: {},
+    user_id: {},
+  },
+  {
+    routine_name: "Intermediate Size",
+    filename: {},
+    user_id: {},
+  },
+  {
+    routine_name: "Intermediate Sharpen",
+    filename: {},
+    user_id: {},
+  },
+  {
+    routine_name: "Advanced Strength",
+    filename: {},
+    user_id: {},
+  },
+  {
+    routine_name: "Advanced Size",
+    filename: {},
+    user_id: {},
+  },
+  {
+    routine_name: "Advanced Sharpen",
+    filename: {},
+    user_id: {},
+  },
+];

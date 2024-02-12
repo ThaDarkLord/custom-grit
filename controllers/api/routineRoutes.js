294bc3111c2259f78f9d1f88414d332b0a0016f4
ThaDarkLord <daniel.j.sykes92@gmail.com>
2024-02-12 13:58:12 -0500

    rendered all pages cleaned up pathing to ensure render could happen and also renamed ids for better functionality

--- controllers/api/routineRoutes.js
+++ controllers/api/routineRoutes.js
@@ -1,20 +1,37 @@
 const router = require("express").Router();
 const { Routines } = require ('../../models');
+const { route } = require("../homepage");
 
+// router.get('/routine/:id', async (req, res) => {
+//     try {
+//         const routineData = await Routines.findByPk(req.params.id);
+//         console.log(routineData)
+//         res.render('routine', routineData)
+//         res.status(200).json(routineData)
+//     } catch (error) {
+//         res.status(500).json(error)
+//     }
+// });
+// route.get('/routines', (req, res) => {
+//     res.render('routine')
+//   })
+  
 
-router.post('/', async (req, res) => {
-    try {
-        const newRoutine = await Routines.create({
+// router.post('/routine', async (req, res) => {
+//     try {
+//         const newRoutine = await Routines.create({
             
-            // if HIIT Protocol include HIIT, if LISS include LISS protocol
-            // will need to have custom routines made for each selected drop box
-            // suggested option would be a custom routine generator (draggable assignment in form)
+//             // if HIIT Protocol include HIIT, if LISS include LISS protocol
+//             // will need to have custom routines made for each selected drop box
+//             // suggested option would be a custom routine generator (draggable assignment in form)
 
-
-
-        })
-    }
-})
+//         });
+//         res.status(200).json(newRoutine)
+//     } catch (err) {
+//         res.status(400).json(err);
+//     }
+// });
 
 
 // will need a delete path
+module.exports = router ;

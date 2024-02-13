8a2f29983c2a913f0f93f087749b3ba108d0a360
0Clark30 <austinlaw.clark@outlook.com>
2024-02-13 08:52:16 -0600

    more seeds

--- seeds/userData.js
+++ seeds/userData.js
@@ -1,14 +1,36 @@
 const {User} = require('../models');
 
 const userData = [
+  {
+    username: "test",
+    email: "jjwatt@gmail.com",
+    password: "Test123",
+    heightInches: 74,
+    weightLbs: 244,
+  },
+  {
+    username: "hiro",
+    email: "hiro@gmail.com",
+    password: "Password123",
+    heightInches: 74,
+    weightLbs: 244,
+  },
+  {
+    username: "Austin",
+    email: "austin@gmail.com",
+    password: "Password123",
+    heightInches: 74,
+    weightLbs: 244,
+  },
+  {
 
-    {
-        username: "test",
-        email: "jjwatt@gmail.com",
-        password: "Test123",
-        heightInches: 74,
-        weightLbs : 244
-    }
-]
+    username: "Daniel",
+    email: "daniel@gmail.com",
+    password: "Password123",
+    heightInches: 74,
+    weightLbs: 244,
+  
+  }
+];
 const seedUsers = () => User.bulkCreate(userData, {individualHooks: true, returning: true});
 module.exports = seedUsers;
--- seeds/workoutInfo.js
+++ seeds/workoutInfo.js
@@ -6,7 +6,35 @@ const workoutInfoData = [
     muscularGoal: 1,
     cardioGoal: 1,
     availability: 1,
-    user_id: 1
+    user_id: 1,
+  },
+  {
+    experience: 1,
+    muscularGoal: 1,
+    cardioGoal: 1,
+    availability: 1,
+    user_id: 2,
+  },
+  {
+    experience: 3,
+    muscularGoal: 3,
+    cardioGoal: 1,
+    availability: 1,
+    user_id: 3,
+  },
+  {
+    experience: 2,
+    muscularGoal: 2,
+    cardioGoal: 1,
+    availability: 1,
+    user_id: 4,
+  },
+  {
+    experience: 3,
+    muscularGoal: 2,
+    cardioGoal: 1,
+    availability: 1,
+    user_id: 5,
   },
 ];
 const seedWorkoutInfo = () =>

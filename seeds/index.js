8befeb799ea857e269f879a676902d67b1c81570
ThaDarkLord <daniel.j.sykes92@gmail.com>
2024-02-13 12:59:27 -0500

    seed for routnine commented out

--- seeds/index.js
+++ seeds/index.js
@@ -1,12 +1,12 @@
 const sequelize = require("../config/connection");
-const seedRoutines = require("./routinesData");
+// const seedRoutines = require("./routinesData");
 const seedUsers = require('./userData');
 const seedWorkoutInfo = require('./workoutInfo')
 const seedAll = async () => {
   await sequelize.sync({ force: true });
     await seedUsers();
     await seedWorkoutInfo();
-  await seedRoutines();
+  // await seedRoutines();
   
 
   process.exit(0);
--- seeds/routinesData.js
+++ seeds/routinesData.js
@@ -1,39 +1,39 @@
-const {Routines} = require('../models');
+// const {Routines} = require('../models');
 
-const routinesData = [
-  //   {
-  //     routine_name: "Beginner",
-  //     filename: ,
+// const routinesData = [
+//   //   {
+//   //     routine_name: "Beginner",
+//   //     filename: ,
 
-  //   },
-  //   {
-  //     routine_name: "Intermediate Strength",
-  //     filename: {},
-  //   },
-  //   {
-  //     routine_name: "Intermediate Size",
-  //     filename: {},
-  //   },
-  //   {
-  //     routine_name: "Intermediate Sharpen",
-  //     filename: {},
-  //   },
-  {
-    routine_name: "Advanced Strength",
-    filename: "advancedStrength.png",
-    user_id: 1,
-  },
-  {
-    routine_name: "Advanced Size",
-    filename: "advancedSize.png",
-    user_id: 1,
-  },
-  {
-    routine_name: "Advanced Sharpen",
-    filename: "advancedSharpen.png",
-    user_id: 1,
-  },
-];
+//   //   },
+//   //   {
+//   //     routine_name: "Intermediate Strength",
+//   //     filename: {},
+//   //   },
+//   //   {
+//   //     routine_name: "Intermediate Size",
+//   //     filename: {},
+//   //   },
+//   //   {
+//   //     routine_name: "Intermediate Sharpen",
+//   //     filename: {},
+//   //   },
+//   {
+//     routine_name: "Advanced Strength",
+//     filename: "advancedStrength.png",
+//     user_id: 1,
+//   },
+//   {
+//     routine_name: "Advanced Size",
+//     filename: "advancedSize.png",
+//     user_id: 1,
+//   },
+//   {
+//     routine_name: "Advanced Sharpen",
+//     filename: "advancedSharpen.png",
+//     user_id: 1,
+//   },
+// ];
 
-const seedRoutines = () => Routines.bulkCreate(routinesData);
-module.exports = seedRoutines;
+// const seedRoutines = () => Routines.bulkCreate(routinesData);
+// module.exports = seedRoutines;

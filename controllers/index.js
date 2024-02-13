cd51a64471dc29eafaa3ab5ea6e683c3a80c29d9
ThaDarkLord <daniel.j.sykes92@gmail.com>
2024-02-13 10:27:41 -0500

    added readme info

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,7 @@
 const router = require('express').Router();
 const homePage = require('./homepage');
 const apiRoutes = require("./api");
-// we will need to add for all of our pages here
-// ./api to get into the api folder which will hold all data
+
 
 
 router.use('/', homePage);
